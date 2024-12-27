c41c40df0c4eaf70223d9b312e2281dd0b220e72
boxcartenant <zactheslade@gmail.com>
2024-12-27 19:22:55 +0000

    idk what I am doing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,20 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.title("My First Streamlit App")
+
+name = st.text_input("What's your name?")
+if name:
+    st.write(f"Hello, {name}!")
+
+if st.button("Click me!"):
+    st.write("Button clicked!")
+
+st.write("This is some text.")
+
+# Example of displaying a dataframe:
+import pandas as pd
+data = {'col1': [1, 2, 3], 'col2': [4, 5, 6]}
+df = pd.DataFrame(data)
+st.dataframe(df)
