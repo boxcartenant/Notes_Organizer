6e866128f7a3a5bbbfc759213b0a6e9c63439d5c
boxcartenant <zactheslade@gmail.com>
2025-02-13 18:53:46 +0000

    time to try to remember how I had login enforcement setup back when it worked....
    I could just go look at an old version (if this doesn't work then I will)

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,8 @@ def main():
     if 'mode' not in st.session_state:
         st.session_state.mode = 1
 
+    logged_in = False
+
     # Sidebar: Mode buttons
     with st.sidebar:
         st.title("Navigation")
@@ -25,18 +27,22 @@ def main():
         if st.button("DB Editor"):
             st.session_state.mode = 3
         st.markdown("---")
+        logged_in = authenticate_user()
+
+
 
     #Everything else
-    match st.session_state.mode:
-        case 1: #book Organizer
-            book_organizer.sidebar()
-            book_organizer.body()
-        case 2: #bibledb to book
-            bibledb_to_book.sidebar()
-            bibledb_to_book.body()
-        case 3: #bibledb_editor
-            bibledb_editor.sidebar()
-            bibledb_editor.body()
+    if logged_in:
+        match st.session_state.mode:
+            case 1: #book Organizer
+                book_organizer.sidebar()
+                book_organizer.body()
+            case 2: #bibledb to book
+                bibledb_to_book.sidebar()
+                bibledb_to_book.body()
+            case 3: #bibledb_editor
+                bibledb_editor.sidebar()
+                bibledb_editor.body()
             
 if __name__ == "__main__":
     main()
