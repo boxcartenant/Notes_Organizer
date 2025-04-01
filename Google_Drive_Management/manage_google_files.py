d81b2553ac7e2454e09ea416313cb06710dc50ad
boxcartenant <zactheslade@gmail.com>
2025-04-01 13:59:54 +0000

    problem with multiple-uploads getting repeated

--- Google_Drive_Management/manage_google_files.py
+++ Google_Drive_Management/manage_google_files.py
@@ -238,6 +238,7 @@ def browse_google_drive(service):
                     logging.info(f"Uploading: {file_name}")
                     upload_file(service, content, file_name, current_uploads_folder_id)
                     st.success(f"Uploaded {file_name} to {current_uploads_folder_name}!")
+                uploaded_files.clear()
 
             # List files in the current uploads folder
             with st.expander("Files", expanded=True):
